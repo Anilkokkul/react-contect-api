5d67b251f8c2333e38722166ef7739ba159ae17a
Anilkokkul <anilkokkul1997@gmail.com>
2023-08-25 18:50:01 +0530

    updated task

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import ProductContextProvider from "./components/context/products.context";
+import CartContextProvider from "./components/context/cart.context";
+import { BrowserRouter } from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <ProductContextProvider>
+      <CartContextProvider>
+        <App />
+      </CartContextProvider>
+    </ProductContextProvider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
